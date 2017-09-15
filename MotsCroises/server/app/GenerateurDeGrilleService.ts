37854472f8f4944c02148abee64c17fc501547bb
joger <jonathan.gervais-ranger@polymtl.ca>
2017-09-15 15:16:03 -0400

    JGR : ajout de la logique qui génère aléatoirement des nombres de mots par ligne|colonne et des grandeurs de mots par ligne|colonne.

--- MotsCroises/server/app/GenerateurDeGrilleService.ts
+++ MotsCroises/server/app/GenerateurDeGrilleService.ts
@@ -1,12 +1,20 @@
 
-import { Grille, Niveau } from './Grille';
+import { Grille, Niveau, DIMENSION_LIGNE_COLONNE } from './Grille';
 import { Mot, Rarete } from './Mot';
 import { EmplacementMot } from './EmplacementMot';
 import { Case, EtatCase } from './Case';
 import { CasePleine } from './CasePleine';
 import { Indice, DifficulteDefinition } from './Indice';
 
-export const lettresDeAlphabet: string = "abcdefghijklmnopqrstuvwxyz";
+export const lettresDeAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+export const nombreLettresDeAlphabet = 26;
+
+export const nombreMotMinimumParLigneOuColonne = 1;
+export const nombreMotMaximumParLigneOuColonne = 2;
+
+export const grandeurMotMinimum = 3;
+export const grandeurMotMaximum = 10;
+export const longueurEspaceNoirEntreDeuxMots = 1;
 
 export class GenerateurDeGrilleService {
 
@@ -26,8 +34,74 @@ export class GenerateurDeGrilleService {
 
     public genereGrilleVide(): Grille {
         const motCroiseVide = new Grille();
+        
+
+        // Pour chaque ligne & colonne, on créer un nombre équivaut aux nombre de mots.
+        const nombreMotsParLigne: number[] = new Array(DIMENSION_LIGNE_COLONNE);
+        const nombreMotsParColonne: number[] = new Array(DIMENSION_LIGNE_COLONNE);
+
+        for(let i = 0; i < DIMENSION_LIGNE_COLONNE; i++) {
+            const grandeurMotLigne: number = this.nombreAleatoireEntreXEtY(nombreMotMinimumParLigneOuColonne,
+                nombreMotMaximumParLigneOuColonne);
+            const grandeurMotColonne:number = this.nombreAleatoireEntreXEtY(nombreMotMinimumParLigneOuColonne,
+                nombreMotMaximumParLigneOuColonne);
+
+            nombreMotsParLigne[i] = grandeurMotLigne;
+            nombreMotsParColonne[i] = grandeurMotColonne;
+        }
+
+        // Pour chaque mot de lignes & colonnes, on créer un nombre équivaut à la longueur du mot. 
+        const grandeurMotsParLigne: number[][] = new Array(DIMENSION_LIGNE_COLONNE);
+        const grandeurMotsParColonne: number[][] = new Array(DIMENSION_LIGNE_COLONNE);
+
+        for(let i = 0; i < DIMENSION_LIGNE_COLONNE; i++) {
+            let grandeurMotLigne: number = this.nombreAleatoireEntreXEtY(grandeurMotMinimum, grandeurMotMaximum);
+            let grandeurMotColonne: number = this.nombreAleatoireEntreXEtY(grandeurMotMinimum, grandeurMotMaximum);
+
+            grandeurMotsParLigne[i].push(grandeurMotLigne);
+            grandeurMotColonne[i].push(grandeurMotColonne);
+
+            let grandeurMaximumDuProchainMot: number;            
+
+            if (nombreMotsParLigne[i] > nombreMotMaximumParLigneOuColonne) {
+
+                if (this.peutAccueillirPlusieursMots(grandeurMotsParLigne[i][0])) {
+                    grandeurMaximumDuProchainMot = grandeurMotMaximum - (grandeurMotsParLigne[i][0] + longueurEspaceNoirEntreDeuxMots);
+                    grandeurMotLigne = this.nombreAleatoireEntreXEtY(grandeurMotMinimum, grandeurMaximumDuProchainMot);
+
+                    grandeurMotsParLigne[i].push(grandeurMotLigne);
+                } else {
+                    // On n'a pas assez d'espace pour ajouter un mot, alors on ajuste le nombre de mots pour cette ligne.
+                    nombreMotsParLigne[i] = 1;
+                }
+
+            }
+
+            if (nombreMotsParColonne[i] > nombreMotMaximumParLigneOuColonne) {
+
+                if (this.peutAccueillirPlusieursMots(grandeurMotsParColonne[i][0])) {
+                    grandeurMaximumDuProchainMot = grandeurMotMaximum - (grandeurMotsParLigne[i][0] + longueurEspaceNoirEntreDeuxMots);
+                    grandeurMotColonne = this.nombreAleatoireEntreXEtY(grandeurMotMinimum, grandeurMaximumDuProchainMot);
+
+                    grandeurMotColonne[i].push(grandeurMotColonne);
+                } else {
+                    // On n'a pas assez d'espace pour ajouter un mot, alors on ajuste le nombre de mots pour cette ligne.
+                    nombreMotsParLigne[i] = 1;
+                }
+
+            }
+
+        }
+
+        // Pour chaque mot de lignes & colonnes, positionnez-le pour que celui-ci est le moins d'intersection possible.
 
         return motCroiseVide;
+    }
+
+    private peutAccueillirPlusieursMots(grandeurDuPremierMot: number): boolean {
+        const grandeurAcceptablePourLePremierMot: number = grandeurMotMaximum - (grandeurMotMinimum + 1);
+
+        return (grandeurDuPremierMot <= grandeurAcceptablePourLePremierMot);
     }
 
     private genereGrilleVideMock(): Grille {
@@ -111,16 +185,16 @@ export class GenerateurDeGrilleService {
 
         for (let emplacementMotCourant of GrillePlein.obtenirPositionsEmplacementsVides()) {
 
-            let motAjoute: boolean = false;
+            let motAjoute = false;
 
             while (!motAjoute) {
                 const grandeur = emplacementMotCourant.obtenirGrandeur();
                 let chaineIdiote = '';
                 for (let i = 0; i < grandeur; i++) {
-                    chaineIdiote = chaineIdiote + lettresDeAlphabet.charAt(this.nombreAleatoireEntre1Et26());
+                    chaineIdiote = chaineIdiote + lettresDeAlphabet.charAt(this.nombreAleatoireEntreXEtY(1, nombreLettresDeAlphabet));
                 }
 
-                let indiceIdiot = new Indice(['definition facile', 'definition un peu difficile', 'definition dure de ouuuuf']);
+                const indiceIdiot = new Indice(['definition facile', 'definition un peu difficile', 'definition dure de ouuuuf']);
                 const motIdiot: Mot = new Mot(chaineIdiote, indiceIdiot);
 
                 if (niveau === Niveau.facile) {
@@ -151,8 +225,8 @@ export class GenerateurDeGrilleService {
         return GrillePlein;
     }
 
-    private nombreAleatoireEntre1Et26(): number {
-        return Math.floor((Math.random() * 26) + 1);
+    private nombreAleatoireEntreXEtY(x: number, y: number): number {
+        return Math.floor((Math.random() * y) + x);
     }
 
 }
--- MotsCroises/server/app/Grille.ts
+++ MotsCroises/server/app/Grille.ts
@@ -4,8 +4,7 @@ import { Case, EtatCase } from './Case';
 import { EmplacementMot } from './EmplacementMot';
 
 
-export const DIMENSION_LIGNE = 10;
-export const DIMENSION_COLONNE = 10;
+export const DIMENSION_LIGNE_COLONNE = 10;
 
 export enum EtatGrille{
     vide,
@@ -28,17 +27,17 @@ export class Grille {
     private etat : EtatGrille;
     private difficulte : Niveau;
     
-    private nombreMotsSurLigne: number[] = new Array(DIMENSION_LIGNE);
-    private nombreMotsSurColonne: number[] = new Array(DIMENSION_COLONNE);
+    private nombreMotsSurLigne: number[] = new Array(DIMENSION_LIGNE_COLONNE);
+    private nombreMotsSurColonne: number[] = new Array(DIMENSION_LIGNE_COLONNE);
 
     public constructor (){
 
         // Instancie la grille vide sans espace noir.
-        for(let i:number = 0; i < DIMENSION_LIGNE; i++) {
+        for(let i:number = 0; i < DIMENSION_LIGNE_COLONNE; i++) {
             this.cases[i] = [];
             this.nombreMotsSurLigne[i] = 0;
 
-            for(let j:number = 0; j < DIMENSION_COLONNE; j++) {                
+            for(let j:number = 0; j < DIMENSION_LIGNE_COLONNE; j++) {                
                 let caseBlanche = new Case(i,j, EtatCase.vide);
                 this.nombreMotsSurColonne[j] = 0;
                 this.cases[i][j] = caseBlanche;
@@ -137,7 +136,7 @@ export class Grille {
 
     public obtenirNombreMotsSurLigne(ligne:number):number {
 
-        if(ligne >= DIMENSION_LIGNE) {
+        if(ligne >= DIMENSION_LIGNE_COLONNE) {
             return -1;
         }
 
@@ -146,7 +145,7 @@ export class Grille {
 
     public obtenirNombreMotsSurColonne(ligne:number) {
 
-        if(ligne >= DIMENSION_LIGNE) {
+        if(ligne >= DIMENSION_LIGNE_COLONNE) {
             return -1;
         }
 
