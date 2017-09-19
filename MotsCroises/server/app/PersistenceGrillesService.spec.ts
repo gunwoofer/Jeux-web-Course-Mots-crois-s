f9e878fda4771b547c7eea431257e08988f307b0
joger <jonathan.gervais-ranger@polymtl.ca>
2017-09-18 22:08:20 -0400

    JGR: Résolution du problème lié au package chai.

--- MotsCroises/server/app/PersistenceGrillesService.spec.ts
+++ MotsCroises/server/app/PersistenceGrillesService.spec.ts
@@ -2,25 +2,18 @@ import { assert } from 'chai';
 import { Niveau, Grille } from './Grille';
 import { GenerateurDeGrilleService } from './GenerateurDeGrilleService';
 import { Observateur, TypeObservateur } from './Observateur';
-//import {PersistenceGrillesService } from './PersistenceGrillesService';
+import {PersistenceGrillesService } from './PersistenceGrillesService';
+import { MongoClient } from 'mongodb';
 
 
-describe('GenerateurDeGrilleService', () => {
+describe('PersistenceGrillesService', () => {
     it('Le serveur genere une grille facile.', () => {
-        //let persistenceGrillesService:PersistenceGrillesService = new PersistenceGrillesService();
-        /*
-        http.get({
-            hostname: 'localhost',
-            port: 3000,
-            path: '/grilles/persistence/grille/facile',
-            agent: false  // create a new agent just for this one request
-          }, (res: any) => {
-            let grille: Grille = res.data;
-            assert(grille.obtenirNiveau() === Niveau.facile);
-          });
-          */
+        let generateur:GenerateurDeGrilleService = new GenerateurDeGrilleService();
+        let persistenceGrillesService:PersistenceGrillesService = new PersistenceGrillesService(generateur);
+        
+        
 
-          assert(true);
+         assert(true);
         
     });
 });
