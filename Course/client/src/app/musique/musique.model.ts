f5a559e5843fa4207b8a4ccdcbb0a694a309ce55
Unknown <younes.ijichi@gmail.com>
2017-10-27 22:51:52 -0400

    YI: Ajout du stinger et de la musique de course avec la condition pour le stinger

--- Course/client/src/app/musique/musique.model.ts
+++ Course/client/src/app/musique/musique.model.ts
@@ -2,6 +2,10 @@ const EMPLACEMENT_MUSIQUE = '../../assets/musiques/';
 const FORMAT_MP3 = '.mp3';
 const NOM_THEMATIQUE = 'Get The New World';
 const NOM_EDITEUR = 'Sims - Building Mode 3';
+const NOM_COURSE = 'The Legend of Zelda Ocarina of Time - Gerudo Valley';
+const NOM_STINGER = 'Zelda - Ocarina of Time - Treasure Chest 1';
+const DEBUT_STINGER = 8;
+const DUREE_STINGER = 12;
 
 export class Musique {
     private musique: HTMLAudioElement;
@@ -40,5 +44,24 @@ export class Musique {
     public lancerMusiqueEditeur(): void {
         this.musique = this.chargerMusique(NOM_EDITEUR);
         this.lancerMusique();
+    }
+
+    public lancerMusiqueCourse(): void {
+        this.musique = this.chargerMusique(NOM_COURSE);
+        this.lancerMusique();
+    }
+
+    public lancerStinger(): void {
+        this.musique = this.chargerMusique(NOM_STINGER);
+        this.musique.loop = false;
+        this.musique.currentTime = DEBUT_STINGER;
+        this.lancerMusique();
+    }
+
+    public conditionStinger(): void {
+        if (this.musique.currentTime > DUREE_STINGER) {
+            this.arreterMusique();
+            this.lancerMusiqueThematique();
+        }
     }
 }
