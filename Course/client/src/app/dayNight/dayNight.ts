6d7f1bf1657ed5212714f59846745366f34c034b
Unknown <anasjmii@gmail.com>
2017-10-30 23:36:12 -0400

    AJ : fonction pour directionalLight

--- Course/client/src/app/dayNight/dayNight.ts
+++ Course/client/src/app/dayNight/dayNight.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HemisphereLight, DirectionalLight, ImageUtils, Scene} from 'three';
+import { HemisphereLight, DirectionalLight, ImageUtils, Scene } from 'three';
 
 
 @Injectable()
@@ -15,5 +15,15 @@ export class GenerateurPisteService {
         this.lumiereHemisphere.groundColor.setHSL(0.095, 0.5, 0.5);
         this.lumiereHemisphere.position.set(0, 500, 0);
         return this.lumiereHemisphere;
+    }
+
+    public lumierDirectionnel(): DirectionalLight {
+
+        this.lumiereDirectionnelle = new DirectionalLight(0xffffff, 1);
+        this.lumiereDirectionnelle.color.setHSL(0.1, 1, 0.95);
+        this.lumiereDirectionnelle.position.set(-1, 0.75, 1);
+        this.lumiereDirectionnelle.position.multiplyScalar(30);
+        this.lumiereDirectionnelle.castShadow = true;
+        return this.lumiereDirectionnelle;
     }
 }
