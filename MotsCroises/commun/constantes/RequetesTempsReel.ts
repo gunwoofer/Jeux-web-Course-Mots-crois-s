88b62e8c3c954c9c7a553dd472dbbd7b4101a810
gunwoofer <valentin.bouis@gmail.com>
2017-11-02 15:55:59 -0400

    VB: Ajout des requetes pour obtenir les mots (cheatmode)

--- MotsCroises/commun/constantes/RequetesTempsReel.ts
+++ MotsCroises/commun/constantes/RequetesTempsReel.ts
@@ -7,7 +7,7 @@ export const REQUETE_SERVEUR_CHANGER_EMPLACEMENT_MOT_SELECTIONNER = 'partie/chan
 export const REQUETE_SERVEUR_OBTENIR_TEMPS_RESTANT = 'partie/tempsrestant';
 export const REQUETE_SERVEUR_OBTENIR_MOTS_TROUVES = 'partie/motstrouve';
 export const REQUETE_SERVEUR_DEMANDE_LISTE_PARTIES_EN_COURS = 'parties/liste';
-
+export const REQUETE_SERVER_OBTENIR_MOTS_COMPLETS_CHEAT_MODE = 'partie/obtenir/motscomplets';
 export const REQUETE_SERVEUR_CREER_PARTIE_MULTIJOUEUR = 'partie/creer/multijoueur';
 export const REQUETE_SERVEUR_JOINDRE_PARTIE = 'partie/joindre';
 
@@ -24,6 +24,6 @@ export const REQUETE_CLIENT_ADVERSAIRE_CHANGER_EMPLACEMENT_MOT_SELECTIONNER = 'p
 export const REQUETE_CLIENT_OBTENIR_TEMPS_RESTANT_RAPPEL = 'partie/tempsrestant/rappel';
 export const REQUETE_CLIENT_OBTENIR_MOTS_TROUVE_RAPPEL = 'partie/motstrouve/rappel';
 export const REQUETE_CLIENT_DEMANDE_LISTE_PARTIES_EN_COURS_RAPPEL = 'parties/liste/rappel';
-
+export const REQUETE_CLIENT_RAPPEL_OBTENIR_MOTS_COMPLETS_CHEAT_MODE = 'partie/obtenir/motscomplets/rappel';
 export const REQUETE_SERVEUR_CREER_PARTIE_MULTIJOUEUR_RAPPEL = 'partie/creer/multijoueur/rappel';
 export const REQUETE_SERVEUR_JOINDRE_PARTIE_RAPPEL = 'partie/joindre/rappel';
