9afb04aa4ed3622530d5b229a347c70ca827bde7
Unknown <anasjmii@gmail.com>
2017-11-03 14:06:58 -0400

    AJ : filtre service

--- Course/client/src/app/filtreCouleur/filtreCouleur.service.ts
+++ Course/client/src/app/filtreCouleur/filtreCouleur.service.ts
@@ -5,12 +5,6 @@ import * as THREE from 'three';
 
 @Injectable()
 export class FiltreCouleurService {
-    private WIDTH = 10000;
-    private HEIGHT = 10000;
-    private arbrePath = '../../assets/objects/arbre/tree.json';
-    private arbreTexture = '../../assets/objects/arbre/tree.jpg';
-    private arbrePath2 = '../../assets/objects/arbre2/tree.json';
-    private arbreTexture2 = '../../assets/objects/arbre2/tree.jpg';
     private container: HTMLDivElement;
     public camera: THREE.PerspectiveCamera;
     public renderer: THREE.WebGLRenderer;
@@ -27,20 +21,18 @@ export class FiltreCouleurService {
         const axesHelper = new THREE.AxisHelper(100);
         this.scene.add(axesHelper);
 
-        // const geometry = new THREE.BoxGeometry(1, 1, 1);
-        // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        // const material2 = new THREE.MeshBasicMaterial({ color: 'red' });
-        // const material3 = new THREE.MeshBasicMaterial({ color: 'blue' });
-        // const cube = new THREE.Mesh(geometry, material);
-        // const cube2 = new THREE.Mesh(geometry, material2);
-        // cube2.position.set(10, 0, 0);
-        // const cube3 = new THREE.Mesh(geometry, material3);
-        // cube3.position.set(-10, 0, 0);
-        // this.scene.add(cube);
-        // this.scene.add(cube2);
-        // this.scene.add(cube3);
-        this.chargerArbre(this.arbrePath, this.arbreTexture);
-        this.creeplane();
+        const geometry = new THREE.BoxGeometry(1, 1, 1);
+        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        const material2 = new THREE.MeshBasicMaterial({ color: 'red' });
+        const material3 = new THREE.MeshBasicMaterial({ color: 'blue' });
+        const cube = new THREE.Mesh(geometry, material);
+        const cube2 = new THREE.Mesh(geometry, material2);
+        cube2.position.set(10, 0, 0);
+        const cube3 = new THREE.Mesh(geometry, material3);
+        cube3.position.set(-10, 0, 0);
+        this.scene.add(cube);
+        this.scene.add(cube2);
+        this.scene.add(cube3);
         this.commencerRendu();
     }
 
@@ -58,18 +50,9 @@ export class FiltreCouleurService {
 
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(30, this.getAspectRatio(), 1, 5000);
-        this.camera.position.z = 1000;
-        this.camera.position.y = 100;
+        this.camera.position.z = 30;
         const vecteur1 = new THREE.Vector3(0, 0, 0);
         this.camera.lookAt(vecteur1);
-    }
-
-    public creeplane(): void {
-        const geometry = new THREE.PlaneGeometry(10000, 10000, 32);
-        const material = new THREE.MeshBasicMaterial({ color: 'pink', side: THREE.DoubleSide });
-        this.plane = new THREE.Mesh(geometry, material);
-        this.plane.rotateX(Math.PI / 2);
-        this.scene.add(this.plane);
     }
 
     public commencerRendu(): void {
@@ -94,40 +77,6 @@ export class FiltreCouleurService {
     public getAspectRatio(): number {
         return this.container.clientWidth / this.container.clientHeight;
     }
-
-
-    public random(min, max): number {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    }
-
-    public genereRandomPosition(vecteur: THREE.Vector3): void {
-        vecteur.x = this.random(-this.WIDTH / 10, this.WIDTH / 10);
-        vecteur.z = this.random(-this.WIDTH / 10, this.WIDTH / 10);
-        vecteur.y = 0;
-    }
-
-    public chargerArbre(path: string, texture: string): void {
-        const loader = new THREE.ObjectLoader();
-        const groupe = new THREE.Object3D();
-        let arbre: any; let lumieres: any; let instance: any;
-        const textur = new THREE.TextureLoader().load(texture);
-        loader.load(path, (obj) => {
-            arbre = obj.children[1];
-            lumieres = obj.children[0];
-            arbre.material.map = textur;
-            for (let i = 0; i < 4; i++) {
-                instance = arbre.clone();
-                this.genereRandomPosition(instance.position);
-                groupe.add(instance);
-            }
-            this.scene.add(lumieres);
-        });
-        this.scene.add(groupe);
-    }
-
-
-
-    //////////////////////////////////////// FILTRE /////////
 
     public recupererObjetAvecMateriel(): void {
         let objet: any;
