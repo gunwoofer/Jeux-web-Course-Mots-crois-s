e2346904449df3dd586c90459297dcfb6b5bd3ec
StephanieVanDyke <stephanie.van-dyke@polymtl.ca>
2017-11-03 14:56:07 -0400

    SVD : La ligne est maintenant sur la piste.  Il faudra s'assurer que c'est bien le cas pour toutes les situations

--- Course/client/src/app/piste/segment.model.ts
+++ Course/client/src/app/piste/segment.model.ts
@@ -3,12 +3,13 @@ import * as THREE from 'three';
 import { Piste } from './piste.model';
 
 export const LARGEUR_PISTE = 5;
-export const LARGEUR_LIGNE_ARRIVER = 5;
+export const LARGEUR_LIGNE_ARRIVER = 0.5;
 export const DISTANCE_LIGNE_ARRIVER = 5;
 
 export class Segment {
     public static chargerSegmentsDePiste(piste: Piste): THREE.Mesh[] {
         const segmentsPisteVisuel: THREE.Mesh[] = new Array();
+        const premierSegment = new THREE.PlaneGeometry(1, 1);
 
         for (let i = 0; i < piste.listepositions.length - 1; i++) {
             let A = 1;
@@ -37,23 +38,16 @@ export class Segment {
             geometrie.vertices[3] = new THREE.Vector3(
                 piste.listepositions[i + 1].x - A * LARGEUR_PISTE, piste.listepositions[i + 1].y - B * LARGEUR_PISTE, 0);
             
-  
-            /* const ligneDepart = new THREE.PlaneGeometry(1, 1);
-
-             ligneDepart.vertices[0] = new THREE.Vector3(piste.listepositions[0].x + LARGEUR_PISTE, piste.listepositions[0].y, 0);
-             ligneDepart.vertices[1] = new THREE.Vector3(piste.listepositions[0].x - LARGEUR_PISTE, piste.listepositions[0].y, 0);
-             ligneDepart.vertices[2] = new THREE.Vector3(piste.listepositions[0].x + LARGEUR_PISTE, piste.listepositions[0].y + LARGEUR_LIGNE_ARRIVER, 0);
-             ligneDepart.vertices[3] = new THREE.Vector3(piste.listepositions[0].x - LARGEUR_PISTE, piste.listepositions[0].y + LARGEUR_LIGNE_ARRIVER, 0);
-               */   
-           // const patch = new THREE.CircleBufferGeometry(5, 1);
-         /*  const patch = new THREE.PlaneGeometry(1, 1);
+                if(i===0)
+                { 
+                    
+                    premierSegment.vertices[0] = geometrie.vertices[0];
+                    premierSegment.vertices[1] = geometrie.vertices[1];
+                    premierSegment.vertices[2] = geometrie.vertices[2];
+                    premierSegment.vertices[3] = geometrie.vertices[3];
+                }
 
 
-          /*  patch.vertices[0] = new THREE.Vector3(piste.listepositions[0].x + LARGEUR_PISTE, piste.listepositions[0].y, 0);
-            patch.vertices[1] = new THREE.Vector3(piste.listepositions[0].x - LARGEUR_PISTE, piste.listepositions[0].y, 0);
-            patch.vertices[2] = new THREE.Vector3(piste.listepositions[0].x + LARGEUR_PISTE, piste.listepositions[0].y + LARGEUR_LIGNE_ARRIVER, 0);
-            patch.vertices[3] = new THREE.Vector3(piste.listepositions[0].x - LARGEUR_PISTE, piste.listepositions[0].y + LARGEUR_LIGNE_ARRIVER, 0);
-            */
             
             
             const patch = new THREE.CircleBufferGeometry(6.5, 128);
@@ -66,22 +60,34 @@ export class Segment {
 
         }
 
-        // this.ajoutDepart(piste);
-        //const depart = new THREE.Vector3(piste.listepositions[1].x-piste.listepositions[0].x, piste.listepositions[1].y - piste.listepositions[0].y, piste.listepositions[1].z - piste.listepositions[0].z);
-        //var material = new THREE.LineBasicMaterial({ color: 0xff00000 });
-        const loader = new THREE.TextureLoader();
-        const texture = loader.load('../../assets/textures/ligne_depart.jpg');
+        const loaderDepart = new THREE.TextureLoader();
+        const textureDepart = loaderDepart.load('../../assets/textures/ligne_depart.jpg');
         const ligneArriver = new THREE.PlaneGeometry(1, 1);
-       // piste.listepositions[0].x = piste.listepositions[0].x - DISTANCE_LIGNE_ARRIVER;
-       // piste.listepositions[0].y = piste.listepositions[0].y - DISTANCE_LIGNE_ARRIVER;
-        ligneArriver.vertices[0] = new THREE.Vector3((piste.listepositions[0].x + LARGEUR_LIGNE_ARRIVER) + DISTANCE_LIGNE_ARRIVER, (piste.listepositions[0].y - LARGEUR_LIGNE_ARRIVER)+ DISTANCE_LIGNE_ARRIVER, 0);
-        ligneArriver.vertices[1] = new THREE.Vector3((piste.listepositions[0].x - LARGEUR_LIGNE_ARRIVER) + DISTANCE_LIGNE_ARRIVER, (piste.listepositions[0].y - LARGEUR_LIGNE_ARRIVER)+ DISTANCE_LIGNE_ARRIVER, 0);
-        ligneArriver.vertices[2] = new THREE.Vector3((piste.listepositions[0].x + LARGEUR_LIGNE_ARRIVER) - DISTANCE_LIGNE_ARRIVER, (piste.listepositions[0].y + LARGEUR_LIGNE_ARRIVER) + DISTANCE_LIGNE_ARRIVER, 0);
-        ligneArriver.vertices[3] = new THREE.Vector3((piste.listepositions[0].x - LARGEUR_LIGNE_ARRIVER) - DISTANCE_LIGNE_ARRIVER, (piste.listepositions[0].y + LARGEUR_LIGNE_ARRIVER) + DISTANCE_LIGNE_ARRIVER, 0);
-        //  geometry.vertices.push(new THREE.Vector3(-10, 0, 0));
-      // geometry.vertices[4] = new THREE.Vector3(geometry, material);
-      const materiel = new THREE.MeshBasicMaterial( { map: texture} );
-      segmentsPisteVisuel.push(new THREE.Mesh(ligneArriver, materiel));
+
+      
+        
+        ligneArriver.vertices[0] = new THREE.Vector3(premierSegment.vertices[0].x, premierSegment.vertices[0].y, 0);
+        ligneArriver.vertices[1] = new THREE.Vector3((premierSegment.vertices[1].x), (premierSegment.vertices[1].y), 0);
+        ligneArriver.vertices[2] = new THREE.Vector3((premierSegment.vertices[2].x ) , (premierSegment.vertices[2].y) , 0);
+        ligneArriver.vertices[3] = new THREE.Vector3((premierSegment.vertices[3].x), (premierSegment.vertices[3].y), 0);
+
+        ligneArriver.scale(0.7, 0.7, 0);
+        ligneArriver.translate(-8,8,0);
+        //premierSegment.center();
+
+      //  ligneArriver.scale(0.2,0.2,0.2);
+       // ligneArriver.rotateX(Math.PI/2);
+       // ligneArriver.rotateY(Math.PI/2);
+
+      //  ligneArriver.translate(-20,20,0);
+        
+        //ligneArriver.rotateY((3.14)/2);
+        //ligneArriver.rotateZ(0.9);
+
+        const materielDepart = new THREE.MeshBasicMaterial( { map: textureDepart} );
+        segmentsPisteVisuel.push(new THREE.Mesh(ligneArriver, materielDepart));
+
+
 
         return segmentsPisteVisuel;
     }
