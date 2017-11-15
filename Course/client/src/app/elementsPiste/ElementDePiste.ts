48bb6f082c209ecaa6e0633f88f66633bbe2e7fa
Valentin Bouis <valentin.bouis@gmail.com>
2017-11-15 14:54:04 -0500

    VB JGR: Modelisation des elements de piste, obstacle

---
+++ Course/client/src/app/elementsPiste/ElementDePiste.ts
@@ -0,0 +1,35 @@
+
+
+
+export abstract class ElementDePiste {
+    private x: number;
+    private y: number;
+    private z: number;
+    private geometrie: THREE.Geometry;
+    private materiel: THREE.Material;
+    private mesh: THREE.Mesh;
+
+
+    constructor(x: number, y: number, z: number) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public abstract effetSurObstacle(): void;
+
+    public obtenirMesh(): THREE.Mesh {
+        return this.mesh;
+    }
+
+    public genererPositionAleatoire(listeSegments: THREE.Mesh[]): THREE.Vector3 {
+        // Prendre aleatoirement des segments
+        // Pour chaque segment aleatoirement choisi faire :
+        // this.genererPointAleatoireSegment(segmentaleatoire);
+        return;
+    }
+
+    private genererPointAleatoireSegment(segment: THREE.Mesh): THREE.Vector3 {
+        return;
+    }
+}
