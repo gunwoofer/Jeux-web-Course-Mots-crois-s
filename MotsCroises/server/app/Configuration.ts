be8f649888624e0cd0351fa1cb16dc7ed5a0680d
Jonathan Gervais-Ranger <jonathan.gervais-ranger@polymtl.ca>
2017-11-27 21:45:56 -0500

    JGR: bd distant.

--- MotsCroises/server/app/Configuration.ts
+++ MotsCroises/server/app/Configuration.ts
@@ -1,4 +1,4 @@
 // accès mongodb
 export const baseDeDonneesUrlLocal = 'mongodb://localhost:27017/motscroises';    // Local
 export const baseDeDonneesUrlExterne = 'mongodb://LOG2990-10:rK54nG58@parapluie.info.polymtl.ca:27017/LOG2990-10-db';   // Serveur LOG2990
-export const baseDeDonneesUrl = baseDeDonneesUrlLocal; // Utilisez dans l'application.
+export const baseDeDonneesUrl = baseDeDonneesUrlExterne; // Utilisez dans l'application.
