ae3b30eafde5f7fd5d4583b8794ff66c7620c6af
Unknown <younes.ijichi@gmail.com>
2017-11-28 22:28:31 -0500

    YI: Tests grille vide terminés

--- MotsCroises/server/app/GenerateurDeGrilleService.spec.ts
+++ MotsCroises/server/app/GenerateurDeGrilleService.spec.ts
@@ -6,13 +6,6 @@ import { Niveau } from '../../commun/Niveau';
 import { GenerateurDeGrilleVide } from './GenerateurDeGrilleVide';
 
 describe('GenerateurDeGrilleService', () => {
-    it('Une grille est carre et fait dix cases de cote.', () => {
-        const generateurDeGrilleService = new GenerateurDeGrilleService();
-        generateurDeGrilleService.genererGrille(Niveau.facile).then((grille) => {
-            assert(grille.cases.obtenirHauteurCases() === 10);
-            assert(grille.cases.obtenirLongueurCases() === 10);
-        });
-    });
 
     it('Chaque ligne et colonne contient un ou deux mots ecrits de gauche a droite et de haut en bas.', () => {
         const generateurDeGrilleService = new GenerateurDeGrilleService();
@@ -28,26 +21,6 @@ describe('GenerateurDeGrilleService', () => {
         const generateurDeGrilleService = new GenerateurDeGrilleService();
         generateurDeGrilleService.genererGrille(Niveau.facile).then((grille) => {
             assert(!grille.motsComplet.contientMotDuplique());
-        });
-    });
-
-    it('Une grille ne contient pas d\'emplacements de mot qui se chevauchent sur la même colonne ou sur la même ligne.', () => {
-        const generateurDeGrilleService = new GenerateurDeGrilleService();
-        generateurDeGrilleService.genererGrille(Niveau.facile).then((grille) => {
-            let compteur = 0;
-            for (const emplacementAEvaluer of grille.obtenirEmplacementsMot()) {
-                compteur = 0;
-                for (const emplacementCourant of grille.obtenirEmplacementsMot()) {
-                    if ((emplacementAEvaluer.obtenirCaseDebut() === emplacementCourant.obtenirCaseDebut()) &&
-                        (emplacementAEvaluer.obtenirCaseFin() === emplacementCourant.obtenirCaseFin())) {
-                        compteur++;
-                        if (compteur > 1) {
-                            assert(false);
-                        }
-                    }
-                }
-            }
-            assert(true);
         });
     });
 
@@ -89,25 +62,6 @@ describe('GenerateurDeGrilleService', () => {
                 }
             }
         });
-    });
-
-    it('Plusieurs grilles vides différentes doivent pouvoir être généré.', () => {
-        const generateurDeGrilleVide = new GenerateurDeGrilleVide();
-        const grille1: Grille = generateurDeGrilleVide.genereGrilleVide(Niveau.facile);
-        const grille2: Grille = generateurDeGrilleVide.genereGrilleVide(Niveau.facile);
-        let grilleDifferente = false;
-
-        for (let i = 0; i < 10; i++) {
-            for (let j = 0; j < 10; j++) {
-                const caseGrille1: Case = grille1.cases.obtenirCase(i, j);
-                const caseGrille2: Case = grille2.cases.obtenirCase(i, j);
-                if (caseGrille1.etat !== caseGrille2.etat) {
-                    grilleDifferente = true;
-                }
-            }
-        }
-
-        assert(grilleDifferente);
     });
 });
 */
--- MotsCroises/server/app/GenerateurDeGrilleVide.spec.ts
+++ MotsCroises/server/app/GenerateurDeGrilleVide.spec.ts
@@ -1,3 +1,4 @@
+import { Case } from './../../commun/Case';
 import { assert } from 'chai';
 import { Grille } from './Grille';
 import { GenerateurDeGrilleVide } from './GenerateurDeGrilleVide';
@@ -9,5 +10,43 @@ describe('GenerateurDeGrilleVide', () => {
         const grille: Grille = generateurDeGrilleVide.genereGrilleVide(Niveau.facile);
         assert(grille.cases.obtenirHauteurCases() === 10);
         assert(grille.cases.obtenirLongueurCases() === 10);
+    });
+
+    it('Une grille ne contient pas d\'emplacements de mot qui se chevauchent sur la même colonne ou sur la même ligne.', () => {
+        const generateurDeGrilleVide = new GenerateurDeGrilleVide();
+        const grille: Grille = generateurDeGrilleVide.genereGrilleVide(Niveau.facile);
+        let compteur = 0;
+        for (const emplacementAEvaluer of grille.obtenirEmplacementsMot()) {
+            compteur = 0;
+            for (const emplacementCourant of grille.obtenirEmplacementsMot()) {
+                if ((emplacementAEvaluer.obtenirCaseDebut() === emplacementCourant.obtenirCaseDebut()) &&
+                    (emplacementAEvaluer.obtenirCaseFin() === emplacementCourant.obtenirCaseFin())) {
+                    compteur++;
+                    if (compteur > 1) {
+                        assert(false);
+                    }
+                }
+            }
+        }
+        assert(true);
+    });
+
+    it('Plusieurs grilles vides différentes doivent pouvoir être généré.', () => {
+        const generateurDeGrilleVide = new GenerateurDeGrilleVide();
+        const grille1: Grille = generateurDeGrilleVide.genereGrilleVide(Niveau.facile);
+        const grille2: Grille = generateurDeGrilleVide.genereGrilleVide(Niveau.facile);
+        let grilleDifferente = false;
+
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+                const caseGrille1: Case = grille1.cases.obtenirCase(i, j);
+                const caseGrille2: Case = grille2.cases.obtenirCase(i, j);
+                if (caseGrille1.etat !== caseGrille2.etat) {
+                    grilleDifferente = true;
+                }
+            }
+        }
+
+        assert(grilleDifferente);
     });
 });
