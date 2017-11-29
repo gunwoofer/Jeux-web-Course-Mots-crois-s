1d7d353ccb05fde1e46206cad0253fc2f597997e
Unknown <younes.ijichi@gmail.com>
2017-11-28 23:06:26 -0500

    YI: Fin des tests pour la generation de grille

--- MotsCroises/server/app/GenerateurDeGrilleService.spec.ts
+++ MotsCroises/server/app/GenerateurDeGrilleService.spec.ts
@@ -27,45 +27,35 @@ describe('GenerateurDeGrilleService', () => {
         assert(!grille.motsComplet.contientMotDuplique());
     });
 
-    /*
     it('Les accents, tremas et cedilles sont ignores.', () => {
-        const generateurDeGrilleService = new GenerateurDeGrilleService();
-        generateurDeGrilleService.genererGrille(Niveau.facile).then((grille) => {
-            for (const ligneCasesCourante of grille.cases.obtenirCases()) {
-                for (const caseCourante of ligneCasesCourante) {
-                    if (caseCourante.etat === EtatCase.pleine) {
-                        const lettreCourante: string = caseCourante.obtenirLettre();
-                        assert(lettreCourante === lettreCourante);
-                    }
-                }
+    for (const ligneCasesCourante of grille.cases.obtenirCases()) {
+        for (const caseCourante of ligneCasesCourante) {
+            if (caseCourante.etat === EtatCase.pleine) {
+                const lettreCourante: string = caseCourante.obtenirLettre();
+                assert(lettreCourante === lettreCourante);
             }
-        });
+        }
+    }
     });
 
     it('Les mots avec apostrophes et traits d\'union sont acceptes, mais ces signes sont ignores dans la representation du mot.', () => {
-        const generateurDeGrilleService = new GenerateurDeGrilleService();
-        generateurDeGrilleService.genererGrille(Niveau.facile).then((grille) => {
-            for (const ligneCasesCourante of grille.cases.obtenirCases()) {
-                for (const caseCourante of ligneCasesCourante) {
-                    if (caseCourante.etat === EtatCase.pleine) {
-                        const lettreCourante: string = caseCourante.obtenirLettre();
-                        assert((lettreCourante !== '-') && (lettreCourante !== '\''));
-                    }
+        for (const ligneCasesCourante of grille.cases.obtenirCases()) {
+            for (const caseCourante of ligneCasesCourante) {
+                if (caseCourante.etat === EtatCase.pleine) {
+                    const lettreCourante: string = caseCourante.obtenirLettre();
+                    assert((lettreCourante !== '-') && (lettreCourante !== '\''));
                 }
             }
-        });
+        }
     });
 
     it('La grille est pleine: chaque case est soit pleine soit noire', () => {
-        const generateurDeGrilleService = new GenerateurDeGrilleService();
-        generateurDeGrilleService.genererGrille(Niveau.facile).then((grille) => {
-            for (let i = 0; i < 10; i++) {
-                for (let j = 0; j < 10; j++) {
-                    assert((grille.cases.obtenirCase(i, j).etat === EtatCase.noir) ||
-                        (grille.cases.obtenirCase(i, j).etat === EtatCase.pleine));
-                }
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+                assert((grille.cases.obtenirCase(i, j).etat === EtatCase.noir) ||
+                    (grille.cases.obtenirCase(i, j).etat === EtatCase.pleine));
             }
-        });
-    });*/
+        }
+    });
 });
 
