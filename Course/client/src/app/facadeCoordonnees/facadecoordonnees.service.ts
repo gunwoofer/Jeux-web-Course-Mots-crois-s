3d5402cf7d5b044acb5141e82de4d622d91665cb
Unknown <anasjmii@gmail.com>
2017-11-30 00:20:37 -0500

    AJ: modification de facadeCordonneService

--- Course/client/src/app/facadeCoordonnees/facadecoordonnees.service.ts
+++ Course/client/src/app/facadeCoordonnees/facadecoordonnees.service.ts
@@ -2,32 +2,38 @@ import { Injectable } from '@angular/core';
 import * as THREE from 'three';
 
 @Injectable()
+
 export class FacadeCoordonneesService {
 
     public mouse: THREE.Vector2;
 
-    public obtenirIntersection( event, scene: THREE.Scene, camera: THREE.Camera,
-                                renderer: THREE.WebGLRenderer): THREE.Intersection {
+    public obtenirIntersection(event, scene: THREE.Scene, camera: THREE.Camera,
+        renderer: THREE.WebGLRenderer): THREE.Intersection {
+        return this.retournerRayCaster(event, renderer, camera).intersectObjects(scene.children)[0];
+    }
 
-        this.mouse = this.obtenirCoordonnees(event, renderer);
+    private retournerRayCaster(event, renderer: THREE.WebGLRenderer, camera: THREE.Camera): THREE.Raycaster {
+        this.mouse = this.obtenirCoordonneesSouris(event, renderer);
         const rayCaster = new THREE.Raycaster();
         rayCaster.setFromCamera(this.mouse, camera);
 
-        return rayCaster.intersectObjects(scene.children)[0];
+        return rayCaster;
     }
 
-    private obtenirCoordonnees(event: MouseEvent, renderer: THREE.WebGLRenderer): THREE.Vector2 {
+    private obtenirCoordonneesSouris(event: MouseEvent, renderer: THREE.WebGLRenderer): THREE.Vector2 {
         event.preventDefault();
 
-        const rectangle = renderer.domElement.getBoundingClientRect();
-
         return new THREE.Vector2(
-            ((event.clientX - rectangle.left) / (rectangle.right - rectangle.left)) * 2 - 1,
-            -((event.clientY - rectangle.top) / (rectangle.bottom - rectangle.top)) * 2 + 1
+            ((event.clientX - this.clientRect(renderer).left) / (this.clientRect(renderer).right - this.clientRect(renderer).left)) * 2 - 1,
+            -((event.clientY - this.clientRect(renderer).top) / (this.clientRect(renderer).bottom - this.clientRect(renderer).top)) * 2 + 1
         );
     }
 
+    public clientRect(renderer: THREE.WebGLRenderer): ClientRect {
+        return renderer.domElement.getBoundingClientRect();
+    }
+
     public miseAJourMouse(event: MouseEvent, renderer: THREE.WebGLRenderer): void {
-        this.mouse = this.obtenirCoordonnees(event, renderer);
+        this.mouse = this.obtenirCoordonneesSouris(event, renderer);
     }
 }
