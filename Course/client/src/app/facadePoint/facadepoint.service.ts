17b9f2d2afeaea31379442892f7987472d01f0eb
Unknown <anasjmii@gmail.com>
2017-11-30 00:34:03 -0500

    AJ: assurance qualite machin chouette

--- Course/client/src/app/facadePoint/facadepoint.service.ts
+++ Course/client/src/app/facadePoint/facadepoint.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
 import * as THREE from 'three';
 
+export const SIZE_POINT = 5;
+export const CERCLE_RADIUS = 100;
+
 @Injectable()
+
 export class FacadePointService {
 
   private listeErreurCouleur = {
@@ -15,40 +19,40 @@ export class FacadePointService {
   constructor () { this.compteur = 0; }
 
   public creerPoint(coordonnees: THREE.Vector3, couleur: string): THREE.Points {
-    const geometrie = new THREE.Geometry();
-    geometrie.vertices.push(
-      new THREE.Vector3(0, 0, 0)
-    );
-    const materiel = new THREE.PointsMaterial({
-      size: 5,
-      color: couleur,
-      opacity: 1
-    });
-    const point = new THREE.Points(geometrie, materiel);
-    point.position.copy(coordonnees);
-    point.position.z = 0;
-    point.geometry.computeBoundingSphere();
-    point.geometry.boundingSphere.radius = 100;
-    point.name = '' + this.compteur;
-    return point;
+      const geometrie = new THREE.Geometry();
+      geometrie.vertices.push(
+        new THREE.Vector3()
+      );
+      const materiel = new THREE.PointsMaterial({
+        size: SIZE_POINT,
+        color: couleur,
+        opacity: 1
+      });
+      const point = new THREE.Points(geometrie, materiel);
+      point.position.copy(coordonnees);
+      point.position.z = 0;
+      point.geometry.computeBoundingSphere();
+      point.geometry.boundingSphere.radius = CERCLE_RADIUS;
+      point.name = '' + this.compteur;
+      return point;
   }
 
   public actualiserCouleurPoints(points: any[]): void {
-    for (const point of points) {
-      point.material.color.set(this.listeErreurCouleur[point.material.status]);
-      point.material.size = 5;
-    }
+      for (const point of points) {
+        point.material.color.set(this.listeErreurCouleur[point.material.status]);
+        point.material.size = SIZE_POINT;
+      }
   }
 
   public restaurerStatusPoints(points: any[]): void {
-    for (let i = 1; i < points.length; i++) {
-      points[i].material.status = 'normal';
-    }
+      for (let point = 1; point < points.length; point++) {
+        points[point].material.status = 'normal';
+      }
   }
 
   public viderListeDesPoints(points: any[]): void {
-    for (let i = points.length - 1; i >= 0; i--) {
-      points.pop();
-    }
+      for (let point = points.length - 1; point >= 0; point--) {
+        points.pop();
+      }
   }
 }
