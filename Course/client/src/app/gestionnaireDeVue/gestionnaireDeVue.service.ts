8b9899ee91265c91969c3f4fd2055b088affd2e7
Unknown <anasjmii@gmail.com>
2017-11-30 02:37:41 -0500

    AJ: merge et modification de classe camera

--- Course/client/src/app/gestionnaireDeVue/gestionnaireDeVue.service.ts
+++ Course/client/src/app/gestionnaireDeVue/gestionnaireDeVue.service.ts
@@ -1,17 +1,20 @@
-import { DELTA_ZOOM, ZOOM_AVANT, ZOOM_ARRIERE, ORIGINE } from './../constant';
+import {
+    DELTA_ZOOM, ZOOM_AVANT, ZOOM_ARRIERE, ORIGINE, CAMERA_OFFSET_Y,
+    OFFSET_VUE_DESSUS, CAMERA_OFFSET_X, CAMERA_OFFSET_Z,
+    VALEUR_MAX_ZOOM, VALEUR_MIN_ZOOM
+} from './../constant';
 import { Injectable } from '@angular/core';
 import { PerspectiveCamera, Vector3 } from 'three';
 import { Voiture } from './../voiture/Voiture';
 
+
 @Injectable()
 export class GestionnaireDeVue {
+    private etatRetroviseur;
 
-    private offsetVueDessus = 50;
-    private cameraOfssetX = -5;
-    private cameraOfssetY = 2;
-    private cameraOfssetZ = 0;
-    private etatRetroviseur = true;
-
+    constructor() {
+        this.etatRetroviseur = true;
+    }
     public changerEtatRetroviseur(): void {
         this.etatRetroviseur = !this.etatRetroviseur;
     }
@@ -23,11 +26,11 @@ export class GestionnaireDeVue {
     public vueDessus(camera: PerspectiveCamera, voiture: Voiture): void {
         camera.position.y = voiture.obtenirVoiture3D().position.y;
         camera.position.x = voiture.obtenirVoiture3D().position.x;
-        camera.position.z = voiture.obtenirVoiture3D().position.z + this.offsetVueDessus;
+        camera.position.z = voiture.obtenirVoiture3D().position.z + OFFSET_VUE_DESSUS;
     }
 
     public vueTroisiemePersonne(camera: PerspectiveCamera, voiture: Voiture): void {
-        let relativeCameraOffset = new Vector3(this.cameraOfssetX, this.cameraOfssetY, this.cameraOfssetZ);
+        let relativeCameraOffset = new Vector3(CAMERA_OFFSET_X, CAMERA_OFFSET_Y, CAMERA_OFFSET_Z);
         relativeCameraOffset = relativeCameraOffset.applyMatrix4(voiture.obtenirVoiture3D().matrixWorld);
         camera.position.set(relativeCameraOffset.x, relativeCameraOffset.y, relativeCameraOffset.z);
         camera.up = new Vector3(ORIGINE, ORIGINE, ORIGINE + 1);
@@ -43,10 +46,10 @@ export class GestionnaireDeVue {
     }
 
     public zoom(event, camera: PerspectiveCamera): void {
-        if (event.key === ZOOM_AVANT && camera.zoom <= 5) {
+        if (event.key === ZOOM_AVANT && camera.zoom <= VALEUR_MAX_ZOOM) {
             camera.zoom += DELTA_ZOOM;
         }
-        if (event.key === ZOOM_ARRIERE && camera.zoom > 1) {
+        if (event.key === ZOOM_ARRIERE && camera.zoom > VALEUR_MIN_ZOOM) {
             camera.zoom -= DELTA_ZOOM;
         }
     }
