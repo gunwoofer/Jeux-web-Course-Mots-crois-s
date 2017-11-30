67a226ca6f0f4ad7e74630e4bd70e959cdf30a85
Jonathan Gervais-Ranger <jonathan.gervais-ranger@polymtl.ca>
2017-11-29 19:07:57 -0500

    JGR: elements de piste ass qualite.

--- Course/client/src/app/elementsPiste/FabriquantElementDePiste.ts
+++ Course/client/src/app/elementsPiste/FabriquantElementDePiste.ts
@@ -6,40 +6,41 @@ import { NidDePoule } from './NidDePoule';
 import * as THREE from 'three';
 
 export class FabriquantElementDePiste {
-  public static creerNouvelleElementPiste(typeElementPiste: TypeElementPiste, listePosition: THREE.Vector3[],
-    position?: THREE.Vector3): ElementDePiste {
-      if (typeElementPiste === TypeElementPiste.Accelerateur) {
-          return new Accelerateur(listePosition, position);
-      }
-      if (typeElementPiste === TypeElementPiste.FlaqueDEau) {
-          return new FlaqueDEau(listePosition, position);
-      }
-      if (typeElementPiste === TypeElementPiste.NidDePoule) {
-          return new NidDePoule(listePosition, position);
-      }
-  }
 
-  public static rehydrater(source: ElementDePiste, listePosition: THREE.Vector3[]): ElementDePiste {
-    const sourceElementDePiste = source as ElementDePiste;
-    let vraiElementDePiste: ElementDePiste;
+    public static rehydrater(source: ElementDePiste, listePosition: THREE.Vector3[]): ElementDePiste {
+        const sourceElementDePiste = source as ElementDePiste;
+        let vraiElementDePiste: ElementDePiste;
 
-    vraiElementDePiste = FabriquantElementDePiste.creerNouvelleElementPiste(source.typeElementDePiste, listePosition,
-         source.position);
+        vraiElementDePiste = FabriquantElementDePiste.creerNouvelleElementPiste(source.typeElementDePiste, listePosition,
+                                                                                source.position);
 
+        Object.assign(vraiElementDePiste, sourceElementDePiste);
 
-    Object.assign(vraiElementDePiste, sourceElementDePiste);
-    return vraiElementDePiste;
-}
+        return vraiElementDePiste;
+    }
 
-  public static estDeType(type: TypeElementPiste, elementAVerifier: ElementDePiste): boolean {
-      if (type === TypeElementPiste.Accelerateur && elementAVerifier instanceof Accelerateur) {
-          return true;
-      } else if (type === TypeElementPiste.FlaqueDEau && elementAVerifier instanceof FlaqueDEau) {
-          return true;
-      } else if (type === TypeElementPiste.NidDePoule && elementAVerifier instanceof NidDePoule) {
-          return true;
-      }
+    public static creerNouvelleElementPiste(  typeElementPiste: TypeElementPiste, listePosition: THREE.Vector3[],
+                                            position?: THREE.Vector3): ElementDePiste {
+        if (typeElementPiste === TypeElementPiste.Accelerateur) {
+            return new Accelerateur(listePosition, position);
+        }
 
-      return false;
-  }
+        if (typeElementPiste === TypeElementPiste.FlaqueDEau) {
+            return new FlaqueDEau(listePosition, position);
+        }
+
+        if (typeElementPiste === TypeElementPiste.NidDePoule) {
+            return new NidDePoule(listePosition, position);
+        }
+    }
+
+    public static estDeType(type: TypeElementPiste, elementAVerifier: ElementDePiste): boolean {
+        if (type === TypeElementPiste.Accelerateur && elementAVerifier instanceof Accelerateur ||
+            type === TypeElementPiste.FlaqueDEau && elementAVerifier instanceof FlaqueDEau ||
+            type === TypeElementPiste.NidDePoule && elementAVerifier instanceof NidDePoule) {
+            return true;
+        }
+
+        return false;
+    }
 }
