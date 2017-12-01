0a6fb939904435f3eeb94fa1b32818fe0b86ee0d
Jonathan G.-R <jonathan.g-r@outlook.com>
2017-12-01 14:35:06 -0500

    JGR: ass qualite effetsonore.

--- Course/client/src/app/effetSonore/effetSonore.service.ts
+++ Course/client/src/app/effetSonore/effetSonore.service.ts
@@ -3,21 +3,21 @@ import { EffetSonore } from './effetSonore';
 
 @Injectable()
 export class EffetSonoreService {
-  private effetSonoreTableau: EffetSonore[] = new Array<EffetSonore>();
+    private effetSonoreTableau: EffetSonore[] = new Array<EffetSonore>();
 
-  public jouerNouvelEffetSonore(NOM_EFFET_SONORE: string, loop: boolean = false): void {
-    const nouvelEffetSonore = new EffetSonore();
-    this.effetSonoreTableau.push(nouvelEffetSonore);
+    public jouerNouvelEffetSonore(NOM_EFFET_SONORE: string, loop: boolean = false): void {
+        const nouvelEffetSonore = new EffetSonore();
+        this.effetSonoreTableau.push(nouvelEffetSonore);
 
-    nouvelEffetSonore.lancerEffetSonore(NOM_EFFET_SONORE, loop);
+        nouvelEffetSonore.lancerEffetSonore(NOM_EFFET_SONORE, loop);
 
-    this.arreterMusique();
-  }
-
-  private arreterMusique(): void {
-    if (this.effetSonoreTableau.length > 3) {
-        this.effetSonoreTableau[0].arreterMusique();
-        this.effetSonoreTableau.shift();
+        this.arreterMusique();
     }
-  }
+
+    private arreterMusique(): void {
+        if (this.effetSonoreTableau.length > 3) {
+            this.effetSonoreTableau[0].arreterMusique();
+            this.effetSonoreTableau.shift();
+        }
+    }
 }
--- Course/client/src/app/effetSonore/effetSonore.ts
+++ Course/client/src/app/effetSonore/effetSonore.ts
@@ -1,23 +1,29 @@
-const EMPLACEMENT_MUSIQUE = '../../assets/effetsSonores/';
-const FORMAT_MP3 = '.mp3';
 export const NOM_MOTEUR = 'moteur1';
 export const NOM_BOOST = 'boost';
 export const NOM_OBSTACLE = 'obstacle';
 
+const EMPLACEMENT_MUSIQUE = '../../assets/effetsSonores/';
+const FORMAT_MP3 = '.mp3';
 
 export class EffetSonore {
-    private musique: HTMLAudioElement;
-    private enEcoute: boolean;
-    public thematique: boolean;
 
+    private musique: HTMLAudioElement;
+    private enEcoute = false;
 
     public static jouerUnEffetSonore(NOM_EFFET_SONORE: string, loop: boolean = false): void {
         new EffetSonore().lancerEffetSonore(NOM_EFFET_SONORE, loop);
     }
 
-    constructor() {
-        this.enEcoute = false;
-        this.thematique = false;
+    public arreterMusique(): void {
+        if (this.enEcoute) {
+            this.musique.pause();
+            this.enEcoute = false;
+        }
+    }
+
+    public lancerEffetSonore(NOM_MUSIQUE: string, loop: boolean = false): void {
+        this.musique = this.chargerEffetSonore(NOM_MUSIQUE, loop);
+        this.lancerMusique();
     }
 
     private chargerEffetSonore(nom: string, loop: boolean = false): HTMLAudioElement {
@@ -32,17 +38,4 @@ export class EffetSonore {
             this.enEcoute = true;
         }
     }
-
-    public arreterMusique(): void {
-        if (this.enEcoute) {
-            this.musique.pause();
-            this.enEcoute = false;
-            this.thematique = false;
-        }
-    }
-
-  public lancerEffetSonore(NOM_MUSIQUE: string, loop: boolean = false): void {
-    this.musique = this.chargerEffetSonore(NOM_MUSIQUE, loop);
-    this.lancerMusique();
-  }
 }
