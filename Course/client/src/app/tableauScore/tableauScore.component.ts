f8379fca60d807bebe2411952c4ecfa53239e8f5
Unknown <anasjmii@gmail.com>
2017-12-04 18:05:56 -0500

    Auto stash before merge of "fixBugJeuxDeCourse" and "origin/fixBugJeuxDeCourse"

--- Course/client/src/app/tableauScore/tableauScore.component.ts
+++ Course/client/src/app/tableauScore/tableauScore.component.ts
@@ -25,21 +25,21 @@ export class TableauScoreComponent implements OnInit, OnDestroy {
 
     public ngOnInit(): void {
         this.temps = this.tableauScoreService.piste.meilleursTemps;
+        this.finPartie = this.tableauScoreService.finPartie;
         this.traitementDonnee.cinqMeilleurTemps(this.temps);
-
-        if (this.tableauScoreService.finPartie) {
+        if (this.finPartie) {
             this.temps = this.tableauScoreService.produireTableauResultat();
         }
-
         if (this.tableauScoreService.temps) {
             this.meilleurTemps = Math.floor(this.tableauScoreService.temps).toString();
+            this.afficher = true;
         }
     }
 
     public soummettre(f: NgForm): void {
         this.tableauScoreService.mettreAjourTableauMeilleurTemps(new Score(f.value.nom, this.meilleurTemps))
-                                .then(message => console.log(message))
-                                .catch(erreur => console.error(erreur));
+            .then(message => console.log(message))
+            .catch(erreur => console.error(erreur));
         this.afficher = false;
         this.temps = this.tableauScoreService.piste.meilleursTemps;
     }
