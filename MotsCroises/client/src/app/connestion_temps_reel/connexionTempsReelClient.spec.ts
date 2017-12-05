27eb221f8dca09da694d37065c17036be00a8590
Unknown <younes.ijichi@gmail.com>
2017-12-04 21:57:13 -0500

    YI: npm run lint done

--- MotsCroises/client/src/app/connestion_temps_reel/connexionTempsReelClient.spec.ts
+++ MotsCroises/client/src/app/connestion_temps_reel/connexionTempsReelClient.spec.ts
@@ -1,10 +1,4 @@
 import { ConnexionTempsReelClient } from './connexionTempsReelClient';
-import * as requetes from '../../../../commun/constantes/requetesTempsReel';
-import { SpecificationPartie } from '../../../../commun/specificationPartie';
-import { Niveau } from '../../../../commun/niveau';
-import { Joueur } from '../../../../commun/joueur';
-import { TypePartie } from '../../../../commun/typePartie';
-import { RequisPourMotAVerifier } from '../../../../commun/requis/requisPourMotAVerifier';
 
 describe('ConnexionTempsReel', () => {
   it('Une connexion en temps réel peut être créé.', (done) => {
